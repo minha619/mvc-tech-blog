57ce1a18bd2eed25969ac2865dbf6f2aa9fdd002
minha619 <minha619@gmail.com>
2021-11-29 00:24:50 -0500

    singup page add

--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -3,12 +3,12 @@ const { Post } = require('../models');
 const postdata = [
   {
     title: 'In hac habitasse platea dictumst.',
-    post_url: 'http://edublogs.org/non/ligula/pellentesque.js',
+    post_content: 'http://edublogs.org/non/ligula/pellentesque.js',
     user_id: 1
   },
   {
     title: 'Morbi non quam nec dui luctus rutrum.',
-    post_url: 'http://ucla.edu/consequat/nulla.html',
+    post_content: 'http://ucla.edu/consequat/nulla.html',
     user_id: 1
   },
   
